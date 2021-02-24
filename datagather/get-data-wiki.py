bb70b1f1218a16a84906ccbe700d7d1e5e76d363
Sonia Cromp <snc40@pitt.edu>
2021-02-24 12:26:06 -0500

    added json parsing, regex cleaning, 500char chunking

--- datagather/get-data-wiki.py
+++ datagather/get-data-wiki.py
@@ -1,19 +1,21 @@
 import wikipedia
 import requests
 import subprocess
+import re
+import json
 
 if __name__ == '__main__':
     outpath = './'
     langs = wikipedia.languages() #dictionary where key is code, value is language name in that language
     i = 0
     for lang in langs.keys():
-        if i > 2:
+        if i > 5:
             break
         i += 1
         print(lang, langs[lang])
 
         try:
-            # download the dump, save to file
+            # download the dump, save raw to file
             dumpname = lang + 'wiki-latest-pages-articles.xml.bz2'
             dumplink = 'https://dumps.wikimedia.org/' + lang + 'wiki/latest/' + dumpname
             r = requests.get(dumplink, allow_redirects=True)
@@ -24,13 +26,56 @@ if __name__ == '__main__':
                 print('wiki too small')
                 continue
 
-            open('./dumps/'+lang+'-raw.xml.bz2', 'wb').write(r.content)
-            subprocess.call(['sh', './get-data-wiki.sh', './dumps/'+lang+'-raw.xml.bz2', lang])
-            f = open('./extracted/' + lang + '.txt', 'r')
-            l = f.readlines()
-            f.close()
-            l = [w.encode('utf-8').decode('raw_unicode_escape') for w in l]
-            print(l[:10])
-
         except:
             print('empty wiki')
+            continue
+
+        # fname =
+        #remove XML
+        print('remove XML')
+        open('./dumps/'+lang+'-raw.xml.bz2', 'wb').write(r.content)
+        subprocess.call(['sh', './get-data-wiki.sh', './dumps/'+lang+'-raw.xml.bz2', lang])
+        f = open('./extracted/' + lang + '.txt', 'r')
+        l = f.readlines()
+        f.close()
+
+        print('fix encoding')
+        #fix encoding
+        l = [ w.encode('utf-8').decode('raw_unicode_escape') for w in l]
+
+        # parse jsons, do some cleaning of remaining XML junk
+        print('parse jsons')
+        texts = ''
+        for line in l:
+            try:
+                article = json.loads(line)
+                text = re.sub('\n|&lt.*;|http://.*|__.*__', ' ', article['text'])
+                text = re.sub('&amp;', '&', text)
+                if len(text) > 0:
+                    texts = texts + ' ' + text
+            except:
+                continue
+
+        # TODO: tokenize by writing to file so script can read
+        # subprocess.call(['sh', './tokenize.sh', lang, ])
+
+        # split to 500-char chunks
+        print('chunking')
+        chunks = []
+        n = 500
+        print(len(texts))
+        while len(texts) > 500:
+            chunk = texts[:500] # remove 1st and last "word" since it could be cut in middle
+            chunks.append(chunk)
+            texts = texts[500:]
+        if len(texts) > 350:
+            chunks.append(texts)
+
+        # todo: shuffle chunks, then limit to 10000 chunks per language?
+
+        # write to file
+        print('writing')
+        out = '\n'.join(chunks)
+        f = open('./extracted/' + lang + '.txt', 'w')
+        f.writelines(out)
+        f.close()
--- datagather/get-data-wiki.sh
+++ datagather/get-data-wiki.sh
@@ -1,5 +1,6 @@
 #1 fname, 2 lang
-python -m wikiextractor.WikiExtractor $1 --processes 8 -q -o - \
+TOKENIZE=~/Documents/skola/dl/proj/XLM/tools/tokenize.sh
+python -m wikiextractor.WikiExtractor $1 --processes 8 --no-templates -q -o - \
 | sed "/^\s*\$/d" \
 | grep -v "^<doc id=" \
 | grep -v "</doc>\$" \
